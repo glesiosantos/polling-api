e615be25b3f4af42a67bfa523d0e4de5b39a1762
Glêsio Santos <glesioss@gmail.com>
2024-10-27 12:24:54 -0300

    Inicializando projeto polling API

---
+++ src/main/java/services/webplus/polling/api/PollingApiApplication.java
@@ -0,0 +1,13 @@
+package services.webplus.polling.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/services/webplus/polling/api/PollingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package services.webplus.polling.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
